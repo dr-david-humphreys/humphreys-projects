bfac4b90217cdde5d9a031e97c2cda83686f8f82
David Humphreys <david.k.humphreys1@gmail.com>
2024-03-20 13:16:39 -0500

    Completed Command Line exercies DecimalToBinary

--- java/module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ java/module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,9 +1,36 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
+		String input = scanner.nextLine();
+
+		String[] values = input.split(" ");
+
+		for (int i = 0; i < values.length; i++) {
+			int decimal = Integer.parseInt(values[i]);
+			String binary = decimalToBinary(decimal);
+			System.out.println(decimal + " in binary is " + binary);
+		}
+
+		scanner.close();
+	}
+	public static String decimalToBinary(int decimal) {
+		StringBuilder binary = new StringBuilder();
+		if (decimal == 0) {
+			return "0";
+		}
+		while (decimal > 0) {
+			int remainder = decimal % 2;
+			binary.insert(0, remainder);
+			decimal /= 2;
+		}
+		return binary.toString();
 	}
 
 }
