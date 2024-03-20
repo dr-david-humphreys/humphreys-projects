0aaf9d1c4e77a28fa4b69f05ebb8140f38483b61
David Humphreys <david.k.humphreys1@gmail.com>
2024-03-20 11:47:28 -0500

    Completed Command Line exercise Fibonacci

--- java/module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ java/module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,30 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.print("Please enter a number: ");
+		int startingNumber = scanner.nextInt();
+
+		int firstNumber = 0;
+		int secondNumber = 1;
+
+		System.out.print(firstNumber + ", " + secondNumber);
+
+		int nextNumber = firstNumber + secondNumber;
+
+		while (nextNumber <= startingNumber) {
+			System.out.print(", " + nextNumber);
+			firstNumber = secondNumber;
+			secondNumber = nextNumber;
+			nextNumber = firstNumber + secondNumber;
+		}
+
+		scanner.close();
 	}
 
 }
