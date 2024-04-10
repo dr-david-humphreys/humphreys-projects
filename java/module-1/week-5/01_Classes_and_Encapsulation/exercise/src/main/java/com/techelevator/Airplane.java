e966545662a65ef59144947237f37f3b2436cfc1
David Humphreys <david.k.humphreys1@gmail.com>
2024-04-10 17:07:07 -0500

    Most of classes and encapsulation

--- java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
+++ java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
@@ -1,5 +1,18 @@
 package com.techelevator;
 
 public class Airplane {
+    private String planeNumber;
+    private int totalFirstClassSeats;
+    private int bookedFirstClassSeats;
+    private int availableFirstClassSeats = totalFirstClassSeats - bookedFirstClassSeats;
+    private int totalCoachSeats;
+    private int bookedCoachSeats;
+    private int availableCoachSeats = totalCoachSeats - bookedCoachSeats;
+
+    public Airplane(String planeNumber, int totalFirstClassSeats, int totalCoachSeats) {
+        this.planeNumber = planeNumber;
+        this.totalFirstClassSeats = totalFirstClassSeats;
+        this.totalCoachSeats = totalCoachSeats;
+    }
 
 }
--- java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,50 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+    private int earnedMarks;
+    private int possibleMarks;
+    private String submitterName;
+    private String letterGrade;
 
+    public HomeworkAssignment(int possibleMarks, String submitterName){
+        this.possibleMarks = possibleMarks;
+        this.submitterName = submitterName;
+    }
+
+    public int getEarnedMarks() {
+        return earnedMarks;
+    }
+
+    public void setEarnedMarks(int earnedMarks) {
+        this.earnedMarks = earnedMarks;
+        calculateLetterGrade();
+    }
+
+    public int getPossibleMarks() {
+        return possibleMarks;
+    }
+
+    public String getSubmitterName() {
+        return submitterName;
+    }
+
+    public String getLetterGrade() {
+        return letterGrade;
+    }
+
+    private void calculateLetterGrade() {
+        double percentage = (double) earnedMarks / possibleMarks * 100;
+
+        if (percentage >= 90) {
+            letterGrade = "A";
+        } else if (percentage >= 80) {
+            letterGrade = "B";
+        } else if (percentage >= 70) {
+            letterGrade = "C";
+        } else if (percentage >= 60) {
+            letterGrade = "D";
+        } else {
+            letterGrade = "F";
+        }
+    }
 }
