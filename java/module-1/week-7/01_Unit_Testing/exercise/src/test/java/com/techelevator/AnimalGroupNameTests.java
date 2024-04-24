fb7bf539e2955bf8ce18f11c2254cdb50d0856d1
David Humphreys <david.k.humphreys1@gmail.com>
2024-04-24 16:57:16 -0500

    Added test classes, not complete yet

---
+++ java/module-1/week-7/01_Unit_Testing/exercise/src/test/java/com/techelevator/AnimalGroupNameTests.java
@@ -0,0 +1,26 @@
+package com.techelevator;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AnimalGroupNameTests {
+    @Test
+    public void testGetHerd() {
+        AnimalGroupName animalGroupName = new AnimalGroupName();
+
+        assertEquals("Crash", animalGroupName.getHerd("Rhino"));
+        assertEquals("Tower", animalGroupName.getHerd("Giraffe"));
+        assertEquals("Herd", animalGroupName.getHerd("Elephant"));
+        assertEquals("Pride", animalGroupName.getHerd("Lion"));
+        assertEquals("Murder", animalGroupName.getHerd("Crow"));
+        assertEquals("Kit", animalGroupName.getHerd("Pigeon"));
+        assertEquals("Pat", animalGroupName.getHerd("Flamingo"));
+        assertEquals("Herd", animalGroupName.getHerd("Deer"));
+        assertEquals("Pack", animalGroupName.getHerd("Dog"));
+        assertEquals("Float", animalGroupName.getHerd("Crocodile"));
+
+        assertEquals("unknown", animalGroupName.getHerd(""));
+        assertEquals("unknown", animalGroupName.getHerd("walrus"));
+        assertEquals("unknown", animalGroupName.getHerd("elephants"));
+    }
+}
