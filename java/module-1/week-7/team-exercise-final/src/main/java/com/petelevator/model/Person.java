da7892204fdc1a7ee35619f48d88b6cea00e0384
Myron Law <myron@techelevator.com>
2024-04-25 08:49:46 -0400

    Initial push of Module1-Wk8 tutorials and exercises; Wk7 team-exercise-final

---
+++ java/module-1/week-7/team-exercise-final/src/main/java/com/petelevator/model/Person.java
@@ -0,0 +1,57 @@
+package com.petelevator.model;
+
+import java.util.Objects;
+
+public abstract class Person {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return firstName.equals(person.firstName) && lastName.equals(person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+}
