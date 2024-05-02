c8b86c927e01cd6e51d7785af898797284b67610
David Humphreys <david.k.humphreys1@gmail.com>
2024-05-02 17:04:51 -0500

    "Completed FizzWriter exercises"

--- java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,5 +1,8 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FizzWriter {
@@ -14,8 +17,32 @@ public class FizzWriter {
 
 	public void run() {
 		/* Your code goes here */
+		System.out.println("Enter the destination file path to save FizzBuzz numbers: ");
+		String destinationFilePath = userInput.nextLine();
 
+		File destinationFile = new File(destinationFilePath);
 
+		try {
+			writeFizzBuzzToFile(destinationFile);
+			System.out.println("FizzBuzz numbers have been saved to " + destinationFilePath);
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: Invalid destination file path.");
+		}
 	}
 
+	private void writeFizzBuzzToFile(File destinationFile) throws FileNotFoundException {
+		try (PrintWriter writer = new PrintWriter(destinationFile)) {
+			for (int i = 1; i <= 300; i++) {
+				if (i % 3 == 0 && i % 5 == 0) {
+					writer.println("FizzBuzz");
+				} else if (i % 3 == 0) {
+					writer.println("Fizz");
+				} else if (i % 5 == 0) {
+					writer.println("Buzz");
+				} else {
+					writer.println(i);
+				}
+			}
+		}
+	}
 }
