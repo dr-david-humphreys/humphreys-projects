0cf1714911ea6925604920e347d2fce08f5e1ed9
David Humphreys <david.k.humphreys1@gmail.com>
2024-05-02 16:50:56 -0500

    "Completed FindAndReplace exercises"

--- java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,5 +1,8 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FindAndReplace {
@@ -14,8 +17,41 @@ public class FindAndReplace {
 
     public void run() {
         /* Your code goes here */
+        System.out.println("What is the search word?");
+        String searchWord = userInput.nextLine();
 
+        System.out.println("What is the replacement word?");
+        String replacementWord = userInput.nextLine();
 
+        System.out.println("What is the source file?");
+        String sourceFilePath = userInput.nextLine();
+
+        System.out.println("What is the destination file?");
+        String destinationFilePath = userInput.nextLine();
+
+        File sourceFile = new File(sourceFilePath);
+        if (!sourceFile.exists()) {
+            System.out.println("Source file does not exist.");
+            return;
+        }
+
+        try {
+            performFindAndReplace(searchWord, replacementWord, sourceFile,destinationFilePath);
+            System.out.println("Replacement completed");
+        } catch (FileNotFoundException e) {
+            System.out.println("Destination is invalid: " + e.getMessage());
+        }
+    }
+
+    private void performFindAndReplace(String searchWord, String replacementWord, File sourceFile, String destinationFilePath) throws FileNotFoundException {
+        try (Scanner fileScanner = new Scanner(sourceFile);
+             PrintWriter writer = new PrintWriter(destinationFilePath)) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String replaceLine = line.replaceAll(searchWord, replacementWord);
+                writer.println(replaceLine);
+            }
+        }
     }
 
 }
