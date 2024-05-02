53d1964970726af06c65d1e8a25fdc921f72eeab
David Humphreys <david.k.humphreys1@gmail.com>
2024-05-02 15:27:13 -0500

    "Completed WordSearch exercises"

--- java/module-1/week-8/01_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ java/module-1/week-8/01_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,5 +1,8 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class WordSearch {
@@ -15,6 +18,35 @@ public class WordSearch {
 	public void run() {
 		/* Your code goes here */
 
+		System.out.println("What is the fully qualified name of the file that should be searched?");
+		String filePath = userInput.nextLine();
+
+		System.out.println("What is the search word you are looking for?");
+		String searchWord = userInput.nextLine();
+
+		System.out.println("Should the search be case sensitive? (Y/N)");
+		boolean caseSensitive = userInput.nextLine().equalsIgnoreCase("Y");
+
+		try {
+			Scanner fileScanner = new Scanner(new File(filePath));
+			int lineNumber = 0;
+			while (fileScanner.hasNextLine()) {
+				String line = fileScanner.nextLine();
+				lineNumber++;
+				if (caseSensitive) {
+					if (line.contains(searchWord)) {
+						System.out.println(lineNumber + ") " + line);
+					}
+				} else {
+					if (line.toLowerCase().contains(searchWord.toLowerCase())) {
+						System.out.println(lineNumber + ") " + line);
+					}
+				}
+			}
+			fileScanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: " + e.getMessage());
+		}
 
 	}
 
