11e84d4d1ef92fc45cf25127acaf5350b93f8fae
David Humphreys <david.k.humphreys1@gmail.com>
2024-05-15 18:40:53 -0500

    Completed HotelReservation

--- java/module-1/week-10/assessment/src/main/java/com/techelevator/HotelReservation.java
+++ java/module-1/week-10/assessment/src/main/java/com/techelevator/HotelReservation.java
@@ -1,8 +1,65 @@
 package com.techelevator;
 
 public class HotelReservation {
-    /*
-    Fill in the class details here...
-     */
+    private String name;
+    private int numberOfNights;
+    private int nightlyRate;
 
+    //Constructor
+    public HotelReservation(String name, int numberOfNights, int nightlyRate) {
+        this.name = name;
+        this.numberOfNights = numberOfNights;
+        this.nightlyRate = nightlyRate;
+    }
+
+    //Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public int getNightlyRate() {
+        return nightlyRate;
+    }
+
+    public void setNightlyRate(int nightlyRate) {
+        this.nightlyRate = nightlyRate;
+    }
+
+    //Derived attribute estimatedTotal
+    public int getEstimatedTotal() {
+        return numberOfNights * nightlyRate;
+    }
+
+    //Method for calculating actual total
+    public int getActualTotal(boolean requiresCleaning, boolean usedMinibar) {
+        int actualTotal = getEstimatedTotal();
+        if (requiresCleaning) {
+            actualTotal += 25;
+        }
+
+        if (usedMinibar) {
+            actualTotal += 15;
+            if (requiresCleaning) {
+                actualTotal += 25;
+            }
+        }
+        return actualTotal;
+    }
+
+    //Override toString method
+    public String toString() {
+        return name + ":" + getEstimatedTotal();
+    }
 }
