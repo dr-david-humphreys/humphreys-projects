225b8789a770d904ec7c8e2853ba8d3e43e4813d
David Humphreys <david.k.humphreys1@gmail.com>
2024-06-12 16:56:10 -0500

    "Mostly through JdbcMovieDao tests"

--- java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcMovieDao.java
+++ java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcMovieDao.java
@@ -1,7 +1,9 @@
 package com.techelevator.movies.dao;
 
+import com.techelevator.movies.model.Genre;
 import com.techelevator.movies.model.Movie;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -17,22 +19,71 @@ public class JdbcMovieDao implements MovieDao {
 
     @Override
     public List<Movie> getMovies() {
-        return null;
+        List<Movie> movies = new ArrayList<>();
+        String sql = "SELECT * FROM movie;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while (results.next()) {
+            movies.add(mapRowToMovie(results));
+        }
+        return movies;
     }
 
     @Override
     public Movie getMovieById(int id) {
-        return new Movie(-1, "Not implemented yet", "", "", "", "", null, 0, -1, -1);
+        String sql = "SELECT m.*, p.person_name AS director_name FROM movie m " +
+                "JOIN person p ON m.director_id = p.person_id WHERE m.movie_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+
+        if (results.next()) {
+            return mapRowToMovie(results);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public List<Movie> getMoviesByTitle(String title, boolean useWildCard) {
-        return null;
+        List<Movie> movies = new ArrayList<>();
+        String sql = "SELECT * FROM movie WHERE LOWER(title) LIKE LOWER(?)";
+
+        if (useWildCard) {
+            title = "%" + title + "%";
+        }
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, title);
+
+        while (results.next()) {
+            movies.add(mapRowToMovie(results));
+        }
+        return movies;
     }
 
     @Override
     public List<Movie> getMoviesByDirectorNameAndBetweenYears(String directorName, int startYear,
                                                               int endYear, boolean useWildCard) {
-        return null;
+        List<Movie> movies = new ArrayList<>();
+        String sql = "SELECT m.* FROM movie m " +
+                "JOIN person p ON m.director_id = p.person_id " +
+                "Where LOWER (p.person_name) LIKE LOWER(?) " +
+                "AND m.release_date BETWEEN ? AND ? " +
+                "ORDER BY m.release_date ASC";
+
+        if (useWildCard) {
+            directorName = "%" + directorName + "%";
+        }
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, directorName, startYear, endYear);
+
+        while (results.next()) {
+            movies.add(mapRowToMovie(results));
+        }
+        return movies;
+    }
+
+    private Movie mapRowToMovie(SqlRowSet results) {
+        Movie movie = new Movie();
+        movie.setId(results.getInt("movie_id"));
+        movie.setTitle(results.getString("title"));
+        return movie;
     }
 }
