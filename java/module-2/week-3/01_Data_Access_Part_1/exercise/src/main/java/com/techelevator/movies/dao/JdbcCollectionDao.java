835db6c9c9f81aa107c9c7af6f64c33694df9e23
David Humphreys <david.k.humphreys1@gmail.com>
2024-06-12 11:55:38 -0500

    "Completed JdbcCollectionDao tests"

--- java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
+++ java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
@@ -29,16 +29,37 @@ public class JdbcCollectionDao implements CollectionDao{
 
     @Override
     public Collection getCollectionById(int id) {
-        return new Collection(-1, "Not implemented yet");
+        String sql = "SELECT * FROM collection WHERE collection_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+
+        if (results.next()) {
+            return mapRowToCollection(results);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public List<Collection> getCollectionsByName(String name, boolean useWildCard) {
-        return null;
+        List<Collection> collections = new ArrayList<>();
+        String sql = "SELECT * FROM collection WHERE LOWER(collection_name) LIKE LOWER(?)";
+
+        if (useWildCard) {
+            name = "%" + name + "%";
+        }
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, name);
+
+        while (results.next()) {
+            collections.add(mapRowToCollection(results));
+        }
+        return collections;
     }
 
     private Collection mapRowToCollection(SqlRowSet results) {
         Collection collection = new Collection();
+        collection.setId(results.getInt("collection_id"));
+        collection.setName(results.getString("collection_name"));
         return collection;
     }
 }
