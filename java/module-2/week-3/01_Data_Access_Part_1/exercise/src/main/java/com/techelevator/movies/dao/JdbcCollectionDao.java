97a92cef74c4c83c7136522487dfc20cc73a2583
David Humphreys <david.k.humphreys1@gmail.com>
2024-06-12 08:46:02 -0500

    "Started data access part 1"

--- java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
+++ java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
@@ -2,6 +2,7 @@ package com.techelevator.movies.dao;
 
 import com.techelevator.movies.model.Collection;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -17,7 +18,13 @@ public class JdbcCollectionDao implements CollectionDao{
 
     @Override
     public List<Collection> getCollections() {
-        return null;
+        List<Collection> collections = new ArrayList<>();
+        String sql = "SELECT * FROM collection;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while (results.next()) {
+            collections.add(mapRowToCollection(results));
+        }
+        return collections;
     }
 
     @Override
@@ -28,5 +35,10 @@ public class JdbcCollectionDao implements CollectionDao{
     @Override
     public List<Collection> getCollectionsByName(String name, boolean useWildCard) {
         return null;
+    }
+
+    private Collection mapRowToCollection(SqlRowSet results) {
+        Collection collection = new Collection();
+        return collection;
     }
 }
