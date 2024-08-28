c58c5bef8f0aa0d9e8f3a30f7a5e6801487d3177
David Humphreys <david.k.humphreys1@gmail.com>
2024-08-28 18:57:46 -0500

    completed exercise 2

--- java/module-3/week-3/02_JavaScript_Functions/exercise/exercises.js
+++ java/module-3/week-3/02_JavaScript_Functions/exercise/exercises.js
@@ -20,6 +20,13 @@
  * @returns {boolean} true if they are admitted
  */
 
+    function isAdmitted(gpa, satScore = 0, recommendation = false) {
+        if (gpa > 4.0 || satScore > 1300 || (gpa > 3.0 && recommendation) || (satScore > 1200 && recommendation)) {
+            return true;
+        }
+        return false;
+    }
+
 /**
  * Write a function called useParameterToFilterArray that accepts a filter function
  * as a parameter. Use this function to filter unfilteredArray and return the result.
@@ -28,6 +35,10 @@
  * @returns {number[]} the filtered array
  */
 let unfilteredArray = [1, 2, 3, 4, 5, 6];
+
+    function useParameterToFilterArray(filterFunction) {
+        return unfilteredArray.filter(filterFunction);
+    }
 
 /**
  * Write a function called makeNumber that takes two strings
@@ -42,6 +53,10 @@ let unfilteredArray = [1, 2, 3, 4, 5, 6];
  * @returns {number} the resultant number
  */
 
+    function makeNumber(first, second = '') {
+        return Number(first + second);
+    }
+
 /**
  * Write a function called addAll that takes an unknown number of parameters
  * and adds all of them together. Return the sum.
@@ -50,11 +65,27 @@ let unfilteredArray = [1, 2, 3, 4, 5, 6];
  * @returns {number} the sum of all the parameters (or arguments)
  */
 
+    function addAll(...num) {
+        let sum = 0;
+        for (let i = 0; i < num.length; i++) {
+            sum += num[i];
+        }
+        return sum;
+    }
+
 /*
  * Write and document a function called makeHappy that takes
  * an array and prepends 'Happy ' to the beginning of all the
  * words and returns them as a new array. Use the `map` function.
  */
+    /**
+     * 
+     * @param {string[]} words An array of words to prepend 'Happy' to 
+     * @returns {string[]} a new array with 'Happy ' prepended to the front
+     */
+    function makeHappy(words) {
+        return words.map(word => 'Happy ' + word);
+    }
 
 /*
  * Write and document a function called getFullAddressesOfProperties
@@ -76,6 +107,14 @@ let unfilteredArray = [1, 2, 3, 4, 5, 6];
  *
  * Use `map` and an anonymous function.
  */
+    /**
+     * 
+     * @param {Object[]} properties an array containing streetNumber, streetName, streetType, city, state, zip 
+     * @returns {string[]} an array of mailing address strings formatted as 'streetNumber streetName streetType city state zip'
+     */
+    function getFullAddressesOfProperties(properties) {
+        return properties.map(property => `${property.streetNumber} ${property.streetName} ${property.streetType} ${property.city} ${property.state} ${property.zip}`);
+    }
 
 /** 
  * Write and document a function called findLargest that uses `forEach`
@@ -91,6 +130,29 @@ let unfilteredArray = [1, 2, 3, 4, 5, 6];
  * @param {number[]|string[]} searchArray the array to search
  * @returns {number|string} the number or string that is largest
  **/
+
+    function findLargest(searchArray) {
+        if (searchArray.length === 0) {
+            return 'Array is empty';
+        }
+
+        let largest = searchArray[0];
+
+        searchArray.forEach(element => {
+            if (typeof element === 'string' && typeof largest === 'string') {
+                if (element > largest) {
+                    largest = element;
+                }
+            } else if (typeof element === 'number' && typeof largest === 'number') {
+                    if (element > largest) {
+                        largest = element;
+                    }
+            } else {
+                return 'Array has mixed types';
+            }
+        });
+        return largest
+    }
 
 
 /*
@@ -110,3 +172,12 @@ let unfilteredArray = [1, 2, 3, 4, 5, 6];
  *
  * Read the tests to verify you have the correct behavior.
  */
+
+    /**
+     * 
+     * @param {number[][]} arrayOfArrays array of arrays, containing numbers 
+     * @returns {number} the sum of all of the numbers in all of the sub-arrays
+     */
+    function getSumOfSubArrayValues(arrayOfArrays) {
+        return arrayOfArrays.reduce((totalSum, subArray) => totalSum + subArray.reduce((subArraySum, value) => subArraySum + value, 0), 0);
+    }
