0b63de8f776f4fc58ae17aba1b19284825d0a404
David Humphreys <david.k.humphreys1@gmail.com>
2024-09-06 12:47:08 -0500

    completed calculator

--- java/module-3/week-4/02_Event_Handling/exercise/calculator/js/calculator.js
+++ java/module-3/week-4/02_Event_Handling/exercise/calculator/js/calculator.js
@@ -72,29 +72,45 @@ function clear() {
 }
 
 // add event listener for when the DOM is loaded
-document.addEventListener('LOADED_EVENT_GOES_HERE', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
   // set the variable called display declared at the top of this file equal to the display element
   // HINT: use its id #display to get a reference to it
+  display = document.getElementById('display');
 
   // get a reference to all of the numbers
   // loop over each of the numbers
   // add a click event listener to each number to call the function clickNumber
+  const numberButtons = document.querySelectorAll('.number');
+  numberButtons.forEach(button => {
+    button.addEventListener('click', clickNumber);
+  });
 
   // get a reference to the decimal point button
   // add a click event listener to call the function clickNumber
   // the decimal point is part of the number so you can call clickNumber for it 
   // as you would for a number
+  const decimalButton = document.querySelector('.decimal');
+  decimalButton.addEventListener('click', clickNumber);
 
   // get a reference to the all clear button
   // add a click event listener to call the function clear  
+  const allClearButton = document.querySelector('.all-clear');
+  allClearButton.addEventListener('click', clear);
 
   // get a reference to all of the operators;
   // loop over each of the operators
   // add a click event listener to each operator to call the function clickOperator
+  const operatorButtons = document.querySelectorAll('.operator');
+  operatorButtons.forEach(button => {
+    button.addEventListener('click', clickOperator);
+  });
 
   // add click event listener for the equal sign
   // should call the function performOperation
+
+  const equalButton = document.querySelector('.equal-sign');
+  equalButton.addEventListener('click', performOperation);
 
 });
 
