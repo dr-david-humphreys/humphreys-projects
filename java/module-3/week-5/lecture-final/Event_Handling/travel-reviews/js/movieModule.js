7f621285d080331e7ab148603b91805a295eb3d2
Myron Law <myron@techelevator.com>
2024-09-10 09:04:09 -0400

    Initial push of Mod3-Wk6 content; Wk5 lecture-final

---
+++ java/module-3/week-5/lecture-final/Event_Handling/travel-reviews/js/movieModule.js
@@ -0,0 +1,152 @@
+const movies = [
+    {
+        "id": 102222,
+        "title": "4 Months, 3 Weeks and 2 Days",
+        "releaseDate": "05-17-2007",
+        "posterImage": "/images/movies/4_Months_3_Weeks_2_Days_Poster.jpg",
+        "directors": [
+            {
+                "id": 302210,
+                "name": "Cristian Mungiu"
+            }
+        ],
+        "genres": [
+            {
+                "id": 2200,
+                "name": "Foreign"
+            },
+            {
+                "id": 2201,
+                "name": "Drama"
+            }
+        ],
+        "taglines": [
+            "How far would you go for a friend?"
+        ],
+        "summary": "Set in 1987 in communist Romania, the story follows what two college-age women go through when one of them seeks an abortion in a culture where it's illegal. The bleakness of the storyline expresses a dark socio-political critique in the twilight years of a repressive dictatorship."
+    },
+    {
+        "id": 102211,
+        "title": "A Cop Movie",
+        "releaseDate": "06-12-2021",
+        "posterImage": "/images/movies/A_Cop_Movie_Poster.jpg",
+        "directors": [
+            {
+                "id": 302234,
+                "name": "Alonso Ruizpalacios"
+            }
+        ],
+        "genres": [
+            {
+                "id": 2200,
+                "name": "Foreign"
+            },
+            {
+                "id": 2201,
+                "name": "Drama"
+            },
+            {
+                "id": 2208,
+                "name": "Documentary"
+            },
+            {
+                "id": 2205,
+                "name": "Crime"
+            },
+            {
+                "id": 2203,
+                "name": "Thriller"
+            }
+        ],
+        "taglines": [],
+        "summary": "A mix of fact and fiction, documentary and narrative, that tells the tale of Teresa and Montoya, two Mexican police officers whose dreams are dashed by the corruption of their trade and who, eventually, find love and comfort in each other."
+    },
+    {
+        "id": 102216,
+        "title": "A Silent Voice",
+        "releaseDate": "09-17-2016",
+        "posterImage": "/images/movies/A_Silent_Voice_Poster.jpg",
+        "directors": [
+            {
+                "id": 302204,
+                "name": "Naoko Yamada"
+            }
+        ],
+        "genres": [
+            {
+                "id": 2200,
+                "name": "Foreign"
+            },
+            {
+                "id": 2211,
+                "name": "Animation"
+            }
+        ],
+        "taglines": [
+            "Sometimes the answer is as simple as learning to listen..."
+        ],
+        "summary": "A beautifully crafted anime, the story captures a high-school bully’s remorse and despair as he tries to redeem himself from past wrongdoing, demonstrating that even the cruelest among us can become vulnerable to feelings of shame and regret. While it is heart wrenching, the story is also full of hope, showing how to ask for forgiveness, as well as how to give it."
+    },
+    {
+        "id": 102213,
+        "title": "Aquarius",
+        "releaseDate": "05-17-2016",
+        "posterImage": "/images/movies/Aquarius_Poster.jpg",
+        "directors": [
+            {
+                "id": 302200,
+                "name": "Kleber Mendonça Filho"
+            }
+        ],
+        "genres": [
+            {
+                "id": 2200,
+                "name": "Foreign"
+            },
+            {
+                "id": 2201,
+                "name": "Drama"
+            }
+        ],
+        "taglines": [],
+        "summary": "Clara, 65, lives her life to the fullest with her family and friends. A construction company wants her Recife oceanfront condo, as they've already bought all the other in the 3 story building. Clara's staying."
+    },
+    {
+        "id": 102209,
+        "title": "Dear Ex",
+        "releaseDate": "04-22-2018",
+        "posterImage": "/images/movies/Dear_Ex_Poster.jpg",
+        "directors": [
+            {
+                "id": 302231,
+                "name": "Chih-Yen Hsu"
+            },
+            {
+                "id": 302232,
+                "name": "Mag Hsu"
+            }
+        ],
+        "genres": [
+            {
+                "id": 2200,
+                "name": "Foreign"
+            },
+            {
+                "id": 2201,
+                "name": "Drama"
+            },
+            {
+                "id": 2206,
+                "name": "Romance"
+            },
+            {
+                "id": 2203,
+                "name": "Thriller"
+            }
+        ],
+        "taglines": [
+            "Once you collaborate, there's no turning back."
+        ],
+        "summary": "A family drama that explores LGBT+ issues in contemporary Taiwan. A teenage boy and his mother are further driven apart when they find out that his father's life insurance policy is to be received by his lover. As much as it is a movie about how people cope with loss, it’s a powerful, heartwarming, and intimate portrait of relationships and all the obstacles they face."
+    }
+]
