ea62c8b3fdac066c72a66dcb47ba7f1482dea46d
David Humphreys <david.k.humphreys1@gmail.com>
2024-09-18 16:56:55 -0500

    halfway through 02

--- java/module-3/week-6/02_Vue_Event_Handling/exercise/src/components/UserList.vue
+++ java/module-3/week-6/02_Vue_Event_Handling/exercise/src/components/UserList.vue
@@ -3,7 +3,9 @@
     <table id="tblUsers">
       <thead>
         <tr>
-          <th>&nbsp;</th>
+          <th>
+            <input type="checkbox" id="selectAll" @change="toggleSelectAll" />
+          </th>
           <th>First Name</th>
           <th>Last Name</th>
           <th>Username</th>
@@ -14,21 +16,11 @@
       </thead>
       <tbody>
         <tr>
-          <td>
-            <input type="checkbox" id="selectAll" />
-          </td>
-          <td>
-            <input type="text" id="firstNameFilter" v-model="filter.firstName" />
-          </td>
-          <td>
-            <input type="text" id="lastNameFilter" v-model="filter.lastName" />
-          </td>
-          <td>
-            <input type="text" id="usernameFilter" v-model="filter.username" />
-          </td>
-          <td>
-            <input type="text" id="emailFilter" v-model="filter.emailAddress" />
-          </td>
+          <td></td>
+          <td><input type="text" id="firstNameFilter" v-model="filter.firstName" /></td>
+          <td><input type="text" id="lastNameFilter" v-model="filter.lastName" /></td>
+          <td><input type="text" id="usernameFilter" v-model="filter.username" /></td>
+          <td><input type="text" id="emailFilter" v-model="filter.emailAddress" /></td>
           <td>
             <select id="statusFilter" v-model="filter.status">
               <option value>Show All</option>
@@ -36,15 +28,19 @@
               <option value="Inactive">Inactive</option>
             </select>
           </td>
-          <td>&nbsp;</td>
+          <td></td>
         </tr>
         <tr
           v-for="user in filteredList"
-          v-bind:key="user.id"
-          v-bind:class="{ deactivated: user.status === 'Inactive' }"
+          :key="user.id"
+          :class="{ deactivated: user.status === 'Inactive' }"
         >
           <td>
-            <input type="checkbox" v-bind:id="user.id" v-bind:value="user.id" />
+            <input
+              type="checkbox"
+              :value="user.id"
+              v-model="selectedUserIds"
+            />
           </td>
           <td>{{ user.firstName }}</td>
           <td>{{ user.lastName }}</td>
@@ -52,36 +48,38 @@
           <td>{{ user.emailAddress }}</td>
           <td>{{ user.status }}</td>
           <td>
-            <button class="btnActivateDeactivate">Activate or Deactivate</button>
+            <button class="btnActivateDeactivate" @click="toggleStatus(user.id)">
+              {{ user.status === 'Active' ? 'Deactivate' : 'Activate' }}
+            </button>
           </td>
         </tr>
       </tbody>
     </table>
 
     <div class="all-actions">
-      <button>Activate Users</button>
-      <button>Deactivate Users</button>
-      <button>Delete Users</button>
+      <button @click="activateSelected" :disabled="!selectedUserIds.length">Activate Users</button>
+      <button @click="deactivateSelected" :disabled="!selectedUserIds.length">Deactivate Users</button>
+      <button @click="deleteSelected" :disabled="!selectedUserIds.length">Delete Users</button>
     </div>
 
-    <button>Add New User</button>
+    <button @click="toggleForm">Add New User</button>
 
-    <form id="frmAddNewUser">
+    <form id="frmAddNewUser" v-if="showForm" @submit.prevent="addNewUser">
       <div class="field">
         <label for="firstName">First Name:</label>
-        <input type="text" id="firstName" name="firstName" />
+        <input type="text" id="firstName" name="firstName" v-model="newUser.firstName" />
       </div>
       <div class="field">
         <label for="lastName">Last Name:</label>
-        <input type="text" id="lastName" name="lastName" />
+        <input type="text" id="lastName" name="lastName" v-model="newUser.lastName" />
       </div>
       <div class="field">
         <label for="username">Username:</label>
-        <input type="text" id="username" name="username" />
+        <input type="text" id="username" name="username" v-model="newUser.username" />
       </div>
       <div class="field">
         <label for="emailAddress">Email Address:</label>
-        <input type="text" id="emailAddress" name="emailAddress" />
+        <input type="text" id="emailAddress" name="emailAddress" v-model="newUser.emailAddress" />
       </div>
       <button type="submit" class="btn save">Save User</button>
     </form>
@@ -99,7 +97,9 @@ export default {
         emailAddress: "",
         status: ""
       },
+      selectedUserIds: [],
       nextUserId: 7,
+      showForm: false,
       newUser: {
         id: null,
         firstName: "",
@@ -160,11 +160,107 @@ export default {
       ]
     };
   },
-  methods: {
-    getNextUserId() {
-      return this.nextUserId++;
+
+  watch: {
+    selectedUserIds() {
+      this.updateSelectAll();
+    },
+
+    filteredList() {
+      this.updateSelectAll();
     }
   },
+
+  methods: {
+    toggleSelectAll() {
+      if (this.selectAllChecked) {
+        this.selectedUserIds = this.filteredList.map(user => user.id);
+      } else {
+        this.selectedUserIds = [];
+      }
+    },
+
+    updateSelectAll() {
+      const filteredUserIds = this.filteredList.map(user => user.id);
+      this.selectAllChecked = filteredUserIds.length > 0 && filteredUserIds.every(id => this.selectedUserIds.includes(id));
+    },
+
+    toggleForm() {
+      this.showForm = !this.showForm;
+    },
+
+    getNextUserId() {
+      return this.nextUserId++;
+    },
+
+    addNewUser() {
+      this.newUser.id = this.getNextUserId();
+      this.users.push({ ...this.newUser });
+      this.clearForm();
+    },
+
+    clearForm() {
+      this.newUser = {
+        id: null,
+        firstName: "",
+        lastName: "",
+        username: "",
+        emailAddress: "",
+        status: "Active"
+      };
+      this.showForm = false;
+    },
+
+    toggleStatus(userId) {
+      const user = this.users.find((u) => u.id === userId);
+      if (user) {
+        user.status = user.status === "Active" ? "Inactive" : "Active";
+      }
+    },
+
+    activateSelected() {
+      this.selectedUserIds.forEach((id) => {
+        const user = this.users.find((u) => u.id === id);
+        if (user) {
+          user.status = "Active";
+        }
+      });
+      this.clearSelections();
+    },
+
+    deactivateSelected() {
+      this.selectedUserIds.forEach((id) => {
+        const user = this.users.find((u) => u.id === id);
+        if (user) {
+          user.status = "Inactive";
+        }
+      });
+      this.clearSelections();
+    },
+
+    deleteSelected() {
+      this.users = this.users.filter((user) => !this.selectedUserIds.includes(user.id));
+      this.clearSelections();
+    },
+
+    clearSelections() {
+      this.selectedUserIds = [];
+      const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+      checkboxes.forEach((checkbox) => {
+        checkbox.checked = false;
+      });
+    },
+
+    toggleSelectAll(event) {
+      const isChecked = event.target.checked;
+      if (isChecked) {
+        this.selectedUserIds = this.filteredList.map((user) => user.id);
+      } else {
+        this.selectedUserIds = [];
+      }
+    }
+  },
+
   computed: {
     filteredList() {
       let filteredUsers = this.users;
