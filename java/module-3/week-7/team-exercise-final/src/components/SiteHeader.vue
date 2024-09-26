c5d5a74436fac1b210aab16e46990346469a5787
Myron Law <myron@techelevator.com>
2024-09-26 09:33:50 -0400

    Initial push of Mod3-Wk8 new content and Mod3-Wk7 team-exercise-final

---
+++ java/module-3/week-7/team-exercise-final/src/components/SiteHeader.vue
@@ -0,0 +1,37 @@
+<template>
+   <header class="app-header">
+        <div class="logo-container">
+            <img src="../assets/appLogo.png" alt="Movie Review Logo" class="logo">
+            <h1 class="app-title">Movie Reviews App</h1>            
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .app-header {
+        background-color: #1f1f1f;
+        color: white;
+        display: flex;
+        align-items: center;
+        padding: 10px 20px;
+        height: 60px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    .logo-container {
+        display: flex;
+        align-items: center;
+    }
+
+    .logo {
+        height: 40px;
+        width: auto;
+        margin-right: 10px;
+    }
+</style>
---
+++ java/module-3/week-7/team-exercise-final/src/views/AddReviewView.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Add Review</h1>
+  <add-review v-bind:movieId="movieId"/>
+</template>
+
+<script>
+import AddReview from '../components/AddReview.vue';
+
+export default {
+  components: {
+    AddReview
+  },
+  computed: {
+    movieId() {
+      // Get movie id from the URL
+      return this.$route.params.id
+    },
+  }
+};
+</script>
+
+<style>
+</style>
