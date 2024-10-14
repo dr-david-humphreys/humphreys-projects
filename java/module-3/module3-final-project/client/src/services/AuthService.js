a48465e172bdaf616b45acb46fcf2b21a35885c6
David Humphreys <david.k.humphreys1@gmail.com>
2024-10-14 12:20:04 -0500

    added AuthService class to RegisterView

--- java/module-3/module3-final-project/client/src/services/AuthService.js
+++ java/module-3/module3-final-project/client/src/services/AuthService.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
 
+const API_BASE_URL = 'http://localhost:9000/';
+
+class AuthService {
+  register(user) {
+    return axios.post(`${API_BASE_URL}/register`, {
+      username: user.username,
+      name: user.name,
+      password: user.password,
+      address: user.address,
+      city: user.city,
+      stateCode: user.stateCode,
+      zip: user.zip,
+      role: user.role
+    });
+  }
+}
+
 export default {
 
   login(user) {
