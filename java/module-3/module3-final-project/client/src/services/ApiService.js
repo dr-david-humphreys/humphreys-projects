32b9cb0c64f3a59609e8bf5f292faed0ee84501c
David Humphreys <david.k.humphreys1@gmail.com>
2024-10-17 20:10:17 -0500

    FINALLY GOT THE BACK END TO CONNECTgit add -A

--- java/module-3/module3-final-project/client/src/services/ApiService.js
+++ java/module-3/module3-final-project/client/src/services/ApiService.js
@@ -1,39 +1,39 @@
 import axios from "axios";
 
-const http = axios.create({
+/*const http = axios.create({
     baseURL: import.meta.env.VITE_REMOTE_API || "http://localhost:9000",
-});
+});*/
 
 export default {
     addPracticeSession(sessionData) {
-        return http.post('/practice-session', sessionData);
+        return axios.post('/practice-session', sessionData);
     },
 
     getPracticeSessions() {
-        return http.get('/practice-session');
+        return axios.get('/practice-session');
     },
 
     updatePracticeSession(id, sessionData) {
-        return http.put(`/practice-session/${id}`, sessionData);
+        return axios.put(`/practice-session/${id}`, sessionData);
     },
 
     deletePracticeSession(id) {
-        return http.delete(`/practice-session/${id}`);
+        return axios.delete(`/practice-session/${id}`);
     },
 
     addGoal(goalData) {
-        return http.post('/goal', goalData);
+        return axios.post('/goal', goalData);
     },
 
     getGoals() {
-        return http.get('/goal');
+        return axios.get('/goal');
     },
 
     updateGoal(id, goalData) {
-        return http.put(`/goal/${id}`, goalData);
+        return axios.put(`/goal/${id}`, goalData);
     },
 
     deleteGoal(id) {
-        return http.delete(`/goal/${id}`);
+        return axios.delete(`/goal/${id}`);
     }
 };
